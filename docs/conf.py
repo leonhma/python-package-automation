acadba30f24e56821125ed6ef157c789df57e0a2
BuildTools <unconfigured@null.spigotmc.org>
2020-10-24 21:34:01 +0200

    svs

--- docs/conf.py
+++ docs/conf.py
@@ -4,10 +4,6 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath(".."))
-
 project = "<>"
 copyright = "<>"
 author = "<>"
